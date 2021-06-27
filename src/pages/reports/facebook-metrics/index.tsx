afee0ad912947f9c6d17327bfbf9fb4f0047c82b
Jesus Reikel <68043863+jesusr00@users.noreply.github.com>
2021-06-27 19:35:47 -0400

    fix ui report of facebook statistics

--- src/pages/reports/facebook-metrics/index.tsx
+++ src/pages/reports/facebook-metrics/index.tsx
@@ -1,50 +1,57 @@
 import React, {useEffect, useState} from "react";
 import { useAppSelector } from "../../../redux";
 import {
-    Grid,
+    List, ListItem, ListItemIcon, ListItemText,
     Table,
-    TableBody, TableCell,
+    TableBody,
+    TableCell,
     TableContainer,
     TableHead,
     TableRow,
     Typography
 } from "@material-ui/core";
+import {
+
+} from "@material-ui/icons"
 import { useDispatch } from "react-redux";
 import { clearVideos } from "../../../redux/reducers/metrics";
 import PageTitle from "../../../components/PageTitle";
 import Video from "../../../types/Video";
 import {useHistory} from "react-router-dom";
-import {Paths} from "../../index";
+import {Paths} from "../../";
+import useStyles from "./styles"
+
 
 function FacebookMetricsReport(): JSX.Element {
-  const videos = useAppSelector((state) => state.metrics.videos);
-  const dispatch = useDispatch();
-  const history = useHistory()
+    const videos = useAppSelector((state) => state.metrics.videos);
+    const dispatch = useDispatch();
+    const history = useHistory()
+    const classes = useStyles()
 
-  const [totals, setTotals] = useState<Video>({
-      length: 0,
-      reach: 0,
-      views: 0,
-      date:"",
-      demographic: {
-          "F.13-17": 0,
-          "F.18-24": 0,
-          "F.25-34": 0,
-          "F.35-44": 0,
-          "F.45-54": 0,
-          "F.55-64": 0,
-          "F.65+": 0,
-          "M.13-17": 0,
-          "M.18-24": 0,
-          "M.25-34": 0,
-          "M.35-44": 0,
-          "M.45-54": 0,
-          "M.55-64": 0,
-          "M.65+": 0,
-      },
-      rankingByCountry: {},
-      rankingByRegion: {},
-  })
+    const [totals, setTotals] = useState<Video>({
+        length: 0,
+        reach: 0,
+        views: 0,
+        date:"",
+        demographic: {
+            "F.13-17": 0,
+            "F.18-24": 0,
+            "F.25-34": 0,
+            "F.35-44": 0,
+            "F.45-54": 0,
+            "F.55-64": 0,
+            "F.65+": 0,
+            "M.13-17": 0,
+            "M.18-24": 0,
+            "M.25-34": 0,
+            "M.35-44": 0,
+            "M.45-54": 0,
+            "M.55-64": 0,
+            "M.65+": 0,
+        },
+        rankingByCountry: {},
+        rankingByRegion: {},
+    })
 
     const secondsToString = (seconds: number): string => {
         const second = Math.round(seconds % 0x3c).toString();
@@ -54,90 +61,90 @@ function FacebookMetricsReport(): JSX.Element {
         return hour + ":" + minute + ":" + second;
     };
 
-  useEffect(() => {
+    useEffect(() => {
 
-      if (videos.length === 0)
-          history.push(Paths.StatisticsMetrics)
+        if (videos.length > 0){
 
-      const total = videos.reduce((previousValue, currentValue) => {
+            const total = videos.reduce((previousValue, currentValue) => {
 
-          const newCountries:{[key: string]: number} = {}
-          Object
-              .keys(previousValue.rankingByCountry)
-              .forEach(
-                  value => newCountries[value] = previousValue.rankingByCountry[value] + currentValue.rankingByCountry[value]
-              )
+                const newCountries:{[key: string]: number} = {}
+                Object
+                    .keys(previousValue.rankingByCountry)
+                    .forEach(
+                        value => newCountries[value] = previousValue.rankingByCountry[value] + currentValue.rankingByCountry[value]
+                    )
 
-          const newRegion:{[key: string]: number} = {}
-          Object
-              .keys(previousValue.rankingByRegion)
-              .forEach(
-                  value => newRegion[value] = previousValue.rankingByRegion[value] + currentValue.rankingByRegion[value]
-              )
+                const newRegion:{[key: string]: number} = {}
+                Object
+                    .keys(previousValue.rankingByRegion)
+                    .forEach(
+                        value => newRegion[value] = previousValue.rankingByRegion[value] + currentValue.rankingByRegion[value]
+                    )
 
-          return {
-              length: previousValue.length + currentValue.length,
-              views: previousValue.views + currentValue.views,
-              reach: previousValue.reach + currentValue.reach,
-              demographic: {
-                  "F.13-17": previousValue.demographic["F.13-17"] + currentValue.demographic["F.13-17"],
-                  "F.18-24": previousValue.demographic["F.18-24"] + currentValue.demographic["F.18-24"],
-                  "F.25-34": previousValue.demographic["F.25-34"] + currentValue.demographic["F.25-34"],
-                  "F.35-44": previousValue.demographic["F.35-44"] + currentValue.demographic["F.35-44"],
-                  "F.45-54": previousValue.demographic["F.45-54"] + currentValue.demographic["F.45-54"],
-                  "F.55-64": previousValue.demographic["F.55-64"] + currentValue.demographic["F.55-64"],
-                  "F.65+": previousValue.demographic["F.65+"] + currentValue.demographic["F.65+"],
-                  "M.13-17": previousValue.demographic["M.13-17"] + currentValue.demographic["M.13-17"],
-                  "M.18-24": previousValue.demographic["M.18-24"] + currentValue.demographic["M.18-24"],
-                  "M.25-34": previousValue.demographic["M.25-34"] + currentValue.demographic["M.25-34"],
-                  "M.35-44": previousValue.demographic["M.35-44"] + currentValue.demographic["M.35-44"],
-                  "M.45-54": previousValue.demographic["M.45-54"] + currentValue.demographic["M.45-54"],
-                  "M.55-64": previousValue.demographic["M.55-64"] + currentValue.demographic["M.55-64"],
-                  "M.65+": previousValue.demographic["M.65+"] + currentValue.demographic["M.65+"],
-              },
-              rankingByCountry: newCountries,
-              rankingByRegion: newRegion,
-              date: "",
-          }})
+                return {
+                    length: previousValue.length + currentValue.length,
+                    views: previousValue.views + currentValue.views,
+                    reach: previousValue.reach + currentValue.reach,
+                    demographic: {
+                        "F.13-17": previousValue.demographic["F.13-17"] + currentValue.demographic["F.13-17"],
+                        "F.18-24": previousValue.demographic["F.18-24"] + currentValue.demographic["F.18-24"],
+                        "F.25-34": previousValue.demographic["F.25-34"] + currentValue.demographic["F.25-34"],
+                        "F.35-44": previousValue.demographic["F.35-44"] + currentValue.demographic["F.35-44"],
+                        "F.45-54": previousValue.demographic["F.45-54"] + currentValue.demographic["F.45-54"],
+                        "F.55-64": previousValue.demographic["F.55-64"] + currentValue.demographic["F.55-64"],
+                        "F.65+": previousValue.demographic["F.65+"] + currentValue.demographic["F.65+"],
+                        "M.13-17": previousValue.demographic["M.13-17"] + currentValue.demographic["M.13-17"],
+                        "M.18-24": previousValue.demographic["M.18-24"] + currentValue.demographic["M.18-24"],
+                        "M.25-34": previousValue.demographic["M.25-34"] + currentValue.demographic["M.25-34"],
+                        "M.35-44": previousValue.demographic["M.35-44"] + currentValue.demographic["M.35-44"],
+                        "M.45-54": previousValue.demographic["M.45-54"] + currentValue.demographic["M.45-54"],
+                        "M.55-64": previousValue.demographic["M.55-64"] + currentValue.demographic["M.55-64"],
+                        "M.65+": previousValue.demographic["M.65+"] + currentValue.demographic["M.65+"],
+                    },
+                    rankingByCountry: newCountries,
+                    rankingByRegion: newRegion,
+                    date: "",
+                }})
 
-      setTotals(total)
+            setTotals(total)
 
-      return () => {
-          dispatch(clearVideos())
-      }
-  }, []);
+        } else
+            history.push(Paths.StatisticsMetrics)
+        return () => {
+            dispatch(clearVideos())
+        }
+    }, []);
 
-    if (totals.length !== 0)
+    if (videos.length > 0)
         return (
             <>
                 <PageTitle title={"Estadíticas de Videos Seleccionados"}/>
-                <Grid container aria-orientation={"horizontal"} style={{ paddingLeft: "10%", paddingRight: "20%" }}>
-                    <Grid item xs={4} md={6} style={{ padding: 15 }}>
-                        <TableContainer>
-                            <Table title={"Totales"}>
-                                <TableHead>
-                                    <Typography>Totales</Typography>
-                                </TableHead>
-                                <TableBody>
-                                    <TableRow>
-                                        <TableCell>Alcance Total:</TableCell>
-                                        <TableCell>{totals.reach}</TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>Vistas Totales:</TableCell>
-                                        <TableCell>{totals.views}</TableCell>
-                                    </TableRow>
-                                    <TableRow>
-                                        <TableCell>Duración Total:</TableCell>
-                                        <TableCell>{secondsToString(totals.length)}</TableCell>
-                                    </TableRow>
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Grid>
-                    <Grid item md={6} style={{ padding: 15}}>
+                <div className={classes.layout}>
+                    <TableContainer className={classes.container}>
+                        <Table title={"Totales"}>
+                            <TableHead>
+                                <Typography>Totales</Typography>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Alcance Total:</TableCell>
+                                    <TableCell>{totals.reach}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Vistas Totales:</TableCell>
+                                    <TableCell>{totals.views}</TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>Duración Total:</TableCell>
+                                    <TableCell>{secondsToString(totals.length)}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <TableContainer className={classes.container}>
                         <Table>
                             <TableHead>
+                                <Typography>Demografía</Typography>
                                 <TableRow>
                                     <TableCell/>
                                     <TableCell>
@@ -228,8 +235,8 @@ function FacebookMetricsReport(): JSX.Element {
                                 </TableRow>
                             </TableBody>
                         </Table>
-                    </Grid>
-                    <Grid item xs={4} md={6} style={{ padding: 15 }}>
+                    </TableContainer>
+                    <TableContainer className={classes.container}>
                         <Table>
                             <TableHead>
                                 <Typography>5 Paises con mas tiempo de reproducción</Typography>
@@ -252,8 +259,8 @@ function FacebookMetricsReport(): JSX.Element {
                                     })}
                             </TableBody>
                         </Table>
-                    </Grid>
-                    <Grid item xs={4} md={6} style={{ padding: 15 }}>
+                    </TableContainer>
+                    <TableContainer  className={classes.container}>
                         <Table>
                             <TableHead>
                                 <Typography>5 Regiones con mas tiempo de reproducción</Typography>
@@ -274,8 +281,23 @@ function FacebookMetricsReport(): JSX.Element {
                                     })}
                             </TableBody>
                         </Table>
-                    </Grid>
-                </Grid>
+                    </TableContainer>
+                    <List className={classes.container}>
+                        <Typography>Videos</Typography>
+                        {
+                            videos.map((video, index) => (
+                                <ListItem key={index}>
+                                    <ListItemIcon>
+                                        -
+                                    </ListItemIcon>
+                                    <ListItemText>
+                                        {video.title}
+                                    </ListItemText>
+                                </ListItem>
+                            ))
+                        }
+                    </List>
+                </div>
             </>
         );
 
