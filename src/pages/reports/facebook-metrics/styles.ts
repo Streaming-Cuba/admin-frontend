00534e3a8504e99be1053aee2d93d1a7e9ad4880
Arael D. Espinosa <cl8dep@gmail.com>
2021-07-03 21:35:19 -0400

    Contries with flag and redirect if empty

--- src/pages/reports/facebook-metrics/styles.ts
+++ src/pages/reports/facebook-metrics/styles.ts
@@ -3,9 +3,17 @@ import { makeStyles } from "@material-ui/core";
 export default makeStyles(() => ({
   layout: {
     paddingLeft: "20%",
-    paddingRight: "20%"
+    paddingRight: "20%",
   },
   container: {
-    marginTop: "3em"
-  }
+    marginTop: "3em",
+  },
+  flagCell: {
+    display: "flex",
+    alignItems: "center",
+
+    "& img": {
+      marginRight: 5,
+    }
+  },
 }));
--- src/utils/FormatUtils.ts
+++ src/utils/FormatUtils.ts
@@ -18,3 +18,25 @@ export function formatVideoDuration(duration: number) {
   }
   return `${finalDuration} {unit}`;
 }
+
+export function secondsToString(seconds: number): string {
+  const second = Math.round(seconds % 0x3c).toString();
+  const hour = Math.floor(seconds / 0xe10).toString();
+  const minute = (Math.floor(seconds / 0x3c) % 0x3c).toString();
+
+  return hour + ":" + minute + ":" + second;
+}
+
+export function getCountryCode(countryName: string): string | null {
+  const matches = countryName.match(/\([A-Z][A-Z]\)/);
+  if (matches) return matches[0].replaceAll("(", "").replaceAll(")", "");
+  return null;
+}
+
+export function getFlagUrlByCountry(countryName: string): string {
+  const code = getCountryCode(countryName);
+  if (code)
+    return `https://www.countryflags.io/${code}/flat/24.png`;
+
+  return "";
+}
